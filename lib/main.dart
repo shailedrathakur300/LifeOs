109854d08f2c6632fad5c3355b69ce2aefb187d6
Shailendra Thakur <shailendrathakuracademic123@gmail.com>
2025-08-20 10:46:08 +0000

    morning menifesto-new

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Onboarding Flow Demo',
+      title: 'Onboarding',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
